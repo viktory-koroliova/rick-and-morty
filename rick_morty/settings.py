9cedd91ce02a48bfc87bdddf85d04444b3a8176c
Vika <viktorykoroliova@gmail.com>
2023-03-17 01:35:40 +0200

    added celery to settings

--- rick_morty/settings.py
+++ rick_morty/settings.py
@@ -137,3 +137,10 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
 }
+
+# Celery Configuration Options
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_TIMEZONE = "Europe/Kiev"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
