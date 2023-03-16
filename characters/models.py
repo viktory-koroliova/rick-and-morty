fa26743faf29b420217baa0c8820a7a3b0d32758
Vika <viktorykoroliova@gmail.com>
2023-03-16 18:55:07 +0200

    created model Character

--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Character(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        ALIVE = "Alive"
+        DEAD = "Dead"
+        UNKNOWN = "unknown"
+
+    class GenderChoices(models.TextChoices):
+        FEMALE = "Female"
+        MALE = "Male"
+        GENDERLESS = "Genderless"
+        UNKNOWN = "unknown"
+
+    api_id = models.IntegerField()
+    name = models.CharField(max_length=255)
+    status = models.CharField(
+        max_length=55,
+        choices=StatusChoices.choices,
+        default=StatusChoices.UNKNOWN
+    )
+    species = models.CharField(max_length=255)
+    gender = models.CharField(max_length=55, choices=GenderChoices.choices)
+    image = models.URLField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
